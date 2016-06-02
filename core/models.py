536d5861fae953c79eb65edb0bcf368eb8a23903
Alexandr Larionov <alarionov93@yandex.ru>
2016-06-03 01:57:57 +0500

    changed model

--- core/models.py
+++ core/models.py
@@ -10,3 +10,32 @@ class Student(models.Model):
         db_table = 'students'
         verbose_name = 'Студент'
         verbose_name_plural = 'Студенты'
+
+class Teacher(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+
+    class Meta:
+        db_table = 'teachers'
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+
+    class Meta:
+        db_table = 'subjects'
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
+class MarkList(models.Model):
+    id = models.AutoField(primary_key=True)
+    mark = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+    subject = models.ForeignKey('Subject', to_field='id', db_column='subject_id',
+                                unique=False, blank=False, null=False)
+
+    class Meta:
+        db_table = 'subjects'
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
