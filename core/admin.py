22aa171432e29dd5c4e6ad11aae82e79c04743b1
Alexandr Larionov <alarionov93@yandex.ru>
2016-06-05 01:27:25 +0500

    changed model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core import models
 
 # Register your models here.
+
+admin.site.register(models.Student)
+admin.site.register(models.Teacher)
+admin.site.register(models.Subject)
+admin.site.register(models.MarkList)
+admin.site.register(models.Department)
+admin.site.register(models.ControlType)
--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+    birth = models.DateField(unique=False, null=False, blank=False, default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'students'
@@ -14,28 +18,75 @@ class Student(models.Model):
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+    ex = models.IntegerField(unique=False, null=False, blank=False, verbose_name='Опыт')
+    department = models.ForeignKey('Department', to_field='id', db_column='department_id',
+                                   unique=False, blank=False, null=False, default=1)
+    subject = models.ForeignKey('Subject', to_field='id', db_column='subject_id', unique=False, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'teachers'
         verbose_name = 'Преподаватель'
         verbose_name_plural = 'Преподаватели'
 
+
+class Department(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'departments'
+        verbose_name = 'Кафедра'
+        verbose_name_plural = 'Кафедры'
+
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'subjects'
         verbose_name = 'Предмет'
         verbose_name_plural = 'Предметы'
 
+class ControlType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Тип')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'c_types'
+        verbose_name = 'Тип контроля'
+        verbose_name_plural = 'Типы контроля'
+
 class MarkList(models.Model):
     id = models.AutoField(primary_key=True)
-    mark = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Имя')
-    subject = models.ForeignKey('Subject', to_field='id', db_column='subject_id',
+    mark = models.CharField(unique=False, null=False, blank=False, max_length=255, verbose_name='Оценка')
+    teacher = models.ForeignKey('Teacher', to_field='id', db_column='teacher_id',
                                 unique=False, blank=False, null=False)
+    student = models.ForeignKey('Student', to_field='id', db_column='student_id',
+                                unique=False, blank=False, null=False)
+    date = models.DateField(default=timezone.now)
+    c_type = models.ForeignKey('ControlType', to_field='id', db_column='c_type_id',
+                                unique=False, blank=False, null=False, default=1)
+
+    @property
+    def subject(self):
+        return self.teacher.subject
+
+    def __str__(self):
+        return '%s: %s, %s, %s' % (self.c_type, self.subject, self.date, self.mark)
 
     class Meta:
         db_table = 'mark_lists'
-        verbose_name = 'Предмет'
-        verbose_name_plural = 'Предметы'
+        verbose_name = 'Ведомость'
+        verbose_name_plural = 'Ведомости'
